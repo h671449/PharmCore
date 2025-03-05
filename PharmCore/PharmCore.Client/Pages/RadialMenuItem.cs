884625f7b036272a1694024f346f97fb14f2b69b
h671449 <671449@stud.hvl.no>
2025-03-05 10:52:12 +0100

    Add project files.

---
+++ PharmCore/PharmCore.Client/Pages/RadialMenuItem.cs
@@ -0,0 +1,13 @@
+﻿
+
+public class RadialMenuItem
+{
+    public string Label { get; set; } = "";
+    public string Url { get; set; } = "";
+}
+//public class RadialMenuItem
+//{
+//    public string Label { get; set; } = "";
+//    public string Url { get; set; } = "";
+//    public string Style { get; set; } = ""; // CSS styles for positioning
+//}
